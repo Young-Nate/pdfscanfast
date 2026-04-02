87ecef0c6828f4544b539636ef1b82c1f76c93dd
PDF Scan Fast i18n <i18n@pdfscanfast.com>
2026-04-02 12:04:39 +0000

    Add favicon and apple-touch-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://api.fontshare.com" />
         <link
           rel="stylesheet"
